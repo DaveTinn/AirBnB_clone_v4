a022409ca5f33f504938f824691521530030609f
codedave <ayodeji_oloruntoba@yahoo.com>
2024-05-05 15:33:07 +0100

    API status

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,5 +1,12 @@
 window.addEventListener('load', function () {
-  // task 2
+  $.ajax('http://0.0.0.0:5001/api/v1/status/').done(function (data) {
+    if (data.status === 'OK') {
+      #('api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
+
   const amenity_ids = {};
   $('input[type=checkbox]').change(function () {
     if ($(this).prop('checked')) {
@@ -13,4 +20,4 @@ window.addEventListener('load', function () {
       $('div.amenities h4').text(Object.values(amenity_ids).join(', '));
     }
   });
-}); 
+});
